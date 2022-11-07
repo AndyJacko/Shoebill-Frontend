db5a90826bb6bc9d826db0f76c1f7336a4e9eb48
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-11-07 16:33:37 +0000

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Login from "./Components/Login";
 import Register from './Components/Register';
+import Update from './Components/Update';
 
 function App() {
   return (
     <div className="App">
                   <Login />
                   <Register />
+                  <Update />
 
     </div>
   );
