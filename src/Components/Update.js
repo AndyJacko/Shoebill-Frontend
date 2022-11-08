e951e16a04b1bf45bfc59bcbaefbf9d7f8126857
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-11-08 14:04:20 +0000

    .
    .

--- src/Components/Update.js
+++ src/Components/Update.js
@@ -17,30 +17,25 @@ const Update = () => {
             Current Avatar:
             <br>
             </br><img src="https://picsum.photos/200" alt="something"></img>
-            <br></br>
-            <br></br>
             <label><p class="contactform">Username
             <input onClick={("")} placeholder="Username"/>
             </p>
             </label>
 
             <label><p class="contactform">Email
-            <input onClick={("")} placeholder="Email address"/>
+            <br></br><input onClick={("")} placeholder="Email address"/>
             </p>
             </label>
 
             <label><p class="contactform">Password
-                <input onClick={("")} placeholder="Custom avatar URL"/>
+                <input onClick={("")} placeholder="Password"/>
                 </p>
             </label>
 
-            <label><p class="contactform">Custom avatar URL:
-                <input onClick={("")} placeholder="Your name"/></p>
+            <label><p class="contactform">Custom avatar URL
+                <input onClick={("")} placeholder="Custom avatar URL"/></p>
             </label>
-            <br></br>
 
-
-            <br></br>
             <button type="password">Submit</button>
 
         </div>
