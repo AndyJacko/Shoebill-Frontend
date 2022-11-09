5b8794e043cd47fc7fcc5cdbda6f180b16578db6
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-11-09 10:47:41 +0000

    modal
    modal

--- src/Components/Testingmodal.js
+++ src/Components/Testingmodal.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Modal from "react-modal";
 
 const Newbill = () => {
-  const submitHandler = () => {};
+  
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleModal() {
+    setIsOpen(!isOpen);
+  }
 
   return (
-    <form onSubmit = {submitHandler}>
+    <div className="App">
+      <button onClick={toggleModal}>Open modal</button>
 
-<div id="container2"></div><div class="specialheading3">Open modal
-            
-            <button type="password">Open modal</button>
-
-        </div>
-        </form>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={toggleModal}
+        contentLabel="My dialog"
+      >
+        <div>My modal dialog.</div>
+        <button onClick={toggleModal}>Close modal</button>
+      </Modal>
+    </div>
   );
+
+  
+  
+//   const submitHandler = () => {};
+
+//   return (
+//     <form onSubmit = {submitHandler}>
+
+// <div id="container2"></div><div class="specialheading3">Open modal
+            
+//             <button type="password">Open modal</button>
+
+//         </div>
+//         </form>
+//   );
+
+
 };
 
 export default Newbill;
