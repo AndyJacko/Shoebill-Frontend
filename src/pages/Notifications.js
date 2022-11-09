294946ed695eabc6d9c91c23937b2ad4142f27b0
Andy Jacko <wisdom.mcr@gmail.com>
2022-11-09 10:56:59 +0000

    Add dummy text to Notifications page - AJ

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Notifications = () => {
-  return <div>Notifications</div>;
+  return (
+    <>
+      <h2>Notifications</h2>
+      <p>This is a dummy page and may be added to later as a stretch goal.</p>
+    </>
+  );
 };
 
 export default Notifications;
