216ab34e228ef763081848089728bbc5b0efdc3e
Alia <106494326+aliavarzea@users.noreply.github.com>
2022-11-10 13:07:54 +0000

    Revert ""added css to post.js""

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -12,42 +12,27 @@ const Login = () => {
     <div className="">
       <button onClick={toggleModal}>Bark</button>
 
-      <Modal isOpen={isOpen} onRequestClose={toggleModal} contentLabel=""
-        style={{
-          overlay: {
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: 'rgba(255, 255, 255, 0.1)'
-          },
-          content: {
-            position: 'absolute',
-            top: '40px',
-            left: '40px',
-            right: '40px',
-            bottom: '40px',
-            border: '0px solid #ccc',
-            background: 'rgba(255, 255, 255, 0.1)',
-            overflow: 'auto',
-            WebkitOverflowScrolling: 'touch',
-            borderRadius: '0px',
-            outline: 'none',
-            padding: '0px'
-          }
-        }}>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={toggleModal}
+        contentLabel=""
+      >
 
-        <div className="specialheading3">
-          <p class="contactform">Compose bark</p>
-          <p class="contactform">
-            Your post
-            <br></br>
-            <input onClick={""} placeholder="What's new, Shoebill?" />
-          </p>
+<div className="specialheading3">
+          
+        <p class="contactform">Compose bark</p>
+          <p class="contactform">Your post
+            <br></br><input onClick={("")} placeholder="What's new, Shoebill?"/>
+            </p>
+            
+            
 
-          <button onClick={toggleModal}>Bark</button>
-        </div>
+            
+            
+            
+                <button onClick={toggleModal}>Bark</button>
+
+            </div>
       </Modal>
     </div>
   );
